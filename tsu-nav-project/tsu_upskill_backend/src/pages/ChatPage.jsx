d3559f15240c9cc77179932b802775c77de29fcf
ninjadam007 <150099388+ninjadam007@users.noreply.github.com>
2026-02-02 22:16:08 +0700

    Create ChatPage.jsx

---
+++ tsu-nav-project/tsu_upskill_backend/src/pages/ChatPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ChatPage from './pages/ChatPage'; // ปรับ path ถ้าวางไว้ที่อื่น
+
+function App() {
+  return (
+    <div className="App">
+      <ChatPage />
+    </div>
+  );
+}
+
+export default App;
