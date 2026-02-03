268fb52876ec2106a9dce3222cf495cd60bb6284
ninjadam007 <150099388+ninjadam007@users.noreply.github.com>
2026-02-03 12:01:57 +0700

    Update apps.py

--- tsu-nav-project/tsu_upskill_backend/apps/users/apps.py
+++ tsu-nav-project/tsu_upskill_backend/apps/users/apps.py
@@ -3,4 +3,17 @@ from django.apps import AppConfig
 class UsersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.users'
-    verbose_name = 'Users'
+    
+    # ปรับชื่อให้แสดงในหน้า Admin เป็นภาษาไทยที่ดูเป็นทางการ
+    # ช่วยให้แอดมิน James แยกโซนจัดการ "คน" ออกจากโซน "แชท" ได้ง่ายขึ้น
+    verbose_name = 'ระบบจัดการข้อมูลนิสิตและบุคลากร (Users)'
+
+    def ready(self):
+        """
+        หากในอนาคตมีการทำระบบ Profile อัตโนมัติเมื่อสมัครสมาชิก 
+        เราจะนำการ import signals มาไว้ที่นี่ครับ
+        """
+        try:
+            import apps.users.signals
+        except ImportError:
+            pass
