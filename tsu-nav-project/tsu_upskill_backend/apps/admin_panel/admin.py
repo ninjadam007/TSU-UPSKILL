0e2bb3ef7c77999fac3390bdf6e8f4c34ebf442d
ninjadam007 <150099388+ninjadam007@users.noreply.github.com>
2026-02-08 06:59:42 +0700

    Update admin.py

--- tsu-nav-project/tsu_upskill_backend/apps/admin_panel/admin.py
+++ tsu-nav-project/tsu_upskill_backend/apps/admin_panel/admin.py
@@ -1,19 +1,8 @@
 from django.contrib import admin
-from apps.chat.models import Message, PendingAdminQuestion 
 from .models import SystemAnnouncement 
-
-@admin.register(Message)
-class MessageAdmin(admin.ModelAdmin):
-    list_display = ('id', 'sender', 'content', 'created_at')
-    list_filter = ('sender', 'created_at')
-
-@admin.register(PendingAdminQuestion)
-class PendingAdminQuestionAdmin(admin.ModelAdmin):
-    list_display = ('id', 'status', 'created_at', 'answered_at')
-    list_editable = ('status',) 
-    list_filter = ('status', 'created_at')
 
 @admin.register(SystemAnnouncement)
 class SystemAnnouncementAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'is_active', 'created_at')
     list_filter = ('is_active', 'created_at')
+    search_fields = ('title', 'content')
