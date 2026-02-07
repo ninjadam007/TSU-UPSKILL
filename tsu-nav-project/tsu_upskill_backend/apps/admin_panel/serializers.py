0790d16b222ec65d94ee7ec95dbe710ab852b2c4
ninjadam007 <150099388+ninjadam007@users.noreply.github.com>
2026-02-08 06:39:25 +0700

    Update serializers.py

--- tsu-nav-project/tsu_upskill_backend/apps/admin_panel/serializers.py
+++ tsu-nav-project/tsu_upskill_backend/apps/admin_panel/serializers.py
@@ -1,1 +1,13 @@
+from rest_framework import serializers
+from .models import SystemAnnouncement
 
+class SystemAnnouncementSerializer(serializers.ModelSerializer):
+    author_name = serializers.ReadOnlyField(source='author.username')
+
+    class Meta:
+        model = SystemAnnouncement
+        fields = [
+            'id', 'title', 'content', 'created_at', 
+            'is_active', 'author', 'author_name'
+        ]
+        read_only_fields = ['created_at', 'author']
